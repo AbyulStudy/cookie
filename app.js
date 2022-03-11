2f74e73f9064c2791afe72bca439f8d646a88e84
Abyul <kinsng1599@gmail.com>
2022-03-11 19:58:21 +0900

    :sparkles:feat: http / http2 를 이용한 cookie 데이터 처리 코드

--- app.js
+++ app.js
@@ -3,19 +3,51 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
+const cookieParser = require('cookie-parser');
 
-const options = {
-  key: fs.readFileSync(path.join(__dirname, './cert/server.key')),
-  cert: fs.readFileSync(path.join(__dirname, './cert/server.crt')),
-  passphrase: 'test',
-};
+// const options = {
+//   key: fs.readFileSync(path.join(__dirname, './cert/server.key')),
+//   cert: fs.readFileSync(path.join(__dirname, './cert/server.crt')),
+//   passphrase: 'test',
+// };
 
+app.use(cookieParser());
+// const cookieOption = {
+// secure: true,
+// httpOnly: true,
+// domain: '127.0.0.1',
+// path: 'foo/bar',
+// expires: expiryDate
+// };
 app.get('/', (req, res) => {
-  res.cookie('name', 'devbyul');
+  const expiryDate = new Date(Date.now() + 60 * 60 * 1000); // 1 hour
+  res.cookie('default', 'cookie');
+  res.cookie('Expires', 'cookie', { expires: expiryDate }); // 2022-03-11T10:35:03.253Z
+  res.cookie('MaxAge', 'cookie', { maxAge: 60 * 60 * 1000 }); // milliseconds
+  res.cookie('Secure', 'cookie', { secure: true });
+  res.cookie('HttpOnly', 'cookie', { httpOnly: true });
+  res.cookie('Path', 'cookie', { path: '/dev/byul' });
+  res.cookie('Domain', 'cookie', { domain: '127.0.0.1:3000' });
+  console.log('req.cookies : ', req.cookies);
+  console.log('req.headers.cookie : ', req.headers.cookie);
   res.send('cookie');
 });
 
+// error (404, 500)
+app.use((req, res, next) => {
+  res.status(404).send('Sorry cant find taht!');
+});
+app.use((err, req, res, next) => {
+  console.log(err.route);
+  console.error(err.stack);
+  res.status(500).send('Something broke!<br>' + err);
+});
+
 const PORT = 3000;
-spdy.createServer(options, app).listen(PORT, () => {
-  console.log('start!');
+// spdy.createServer(options, app).listen(PORT, () => {
+//   console.log('https://127.0.0.1:3000/');
+// });
+
+app.listen(PORT, () => {
+  console.log('http://127.0.0.1:3000/');
 });
